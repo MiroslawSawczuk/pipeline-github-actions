66f0612f81104d3632c044d599016bb29b5eb13c
MiroslawSawczuk <miroslaw.sawczuk@gmail.com>
2022-12-07 18:37:41 +0100

    build

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>My app</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
